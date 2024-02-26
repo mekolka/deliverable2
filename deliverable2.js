c929510e9f8b2ff11f59394b98c7164caebfe79d
mekolka <158101857+mekolka@users.noreply.github.com>
2024-02-25 22:47:58 -0600

    second commit

--- deliverable2.js
+++ deliverable2.js
@@ -1,1 +1,15 @@
-console.log(SUBWAY)
+document.getElementById('button').addEventListener('click', function() {
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => {
+      const imageContainer = document.getElementById('image_container');
+      imageContainer.innerHTML = '';
+      const img = document.createElement('img');
+      img.src = data.message; 
+      img.alt = 'A random dog';
+      imageContainer.appendChild(img);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+});
